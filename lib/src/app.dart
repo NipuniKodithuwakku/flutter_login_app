f854b780d907a74386f87948eac297af299ab913
Nipuni Kodithuwakku <39560338+NipuniKodithuwakku@users.noreply.github.com>
2020-03-15 23:17:44 +0530

    add email and password textFormFields

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:login_stateful/src/screens/login_screen.dart';
 
 class App extends StatelessWidget{
   Widget build(context){
     return MaterialApp(
       title: 'Login Screen',
       home: Scaffold(
-        body: Text('Login Here!'),
+        body: LoginScreen(),
 
       ),
     );
--- lib/src/screens/login_screen.dart
+++ lib/src/screens/login_screen.dart
@@ -1,23 +1,41 @@
 import 'package:flutter/material.dart';
 
-class LoginScreen extends StatefulWidget{
-  createState(){
+class LoginScreen extends StatefulWidget {
+  createState() {
     return LoginScreenState();
   }
-
 }
 
-class LoginScreenState extends State<LoginScreen>{
-  Widget build (context){
+class LoginScreenState extends State<LoginScreen> {
+  Widget build(context) {
     return Container(
       child: Form(
-        child: Column(
-          children: <Widget>[
-            TextFormField(),
-            TextFormField(),
-            RaisedButton(onPressed: null)
-          ],
-          )),
+          child: Column(
+        children: <Widget>[
+          emailField(),
+          passwordField(),
+          // submitButton(),
+        ],
+      )),
+    );
+  }
+
+  Widget emailField() {
+    return TextFormField(
+      keyboardType: TextInputType.emailAddress,
+      decoration: InputDecoration(
+        labelText: 'Email Address',
+        hintText: 'you@example.com',
+      ),
+    );
+  }
+
+  Widget passwordField() {
+    return TextFormField(
+      decoration: InputDecoration(
+        labelText: 'Password',
+        hintText: 'password',
+      ),
     );
   }
 }
